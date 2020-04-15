7763697af73b3d7f05e85e11820174d36bb68c9c
SaraElmaghraby <58309697+SaraElmaghraby@users.noreply.github.com>
2020-04-15 16:38:31 +0200

    Add project files.

---
+++ ChatTask/ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace ChatTask
+{
+    [HubName("chathub")]
+    public class ChatHub : Hub
+    {
+        //public void Hello()
+        //{
+        //    Clients.All.hello();
+        //}
+        public void send(string name, string message)
+        {
+            Clients.All.newMessage(name, message);
+        }
+        public void joinGroup(string groupname, string name)
+        {
+            Groups.Add(Context.ConnectionId, groupname);
+            Clients.OthersInGroup(groupname).newMember(name);
+        }
+
+        public void sendGroup(string name, string groupName, string message)
+        {
+            Clients.Group(groupName).newGroupMess(name, groupName, message);
+        }
+    }
+}
---
+++ signalrtest/chat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace signalrtest
+{
+    [HubName("chathub")]
+    public class chat : Hub
+    {
+        public void send(string name ,string message)
+        {
+            Clients.All.newMessage(name,message);
+        }
+
+        public void joinGroup(string groupname ,string name)
+        {
+            Groups.Add(Context.ConnectionId, groupname);
+            Clients.OthersInGroup(groupname).newMember(name);
+        }
+
+        public void sendGroup(string name ,string groupName ,string message)
+        {
+            Clients.Group(groupName).newGroupMess(name, groupName, message);
+        }
+    }
+}
